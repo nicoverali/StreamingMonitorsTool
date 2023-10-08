881736d53c6fef6f68bde210ecca29bac97edfb6
Nicolas A. Vera <nicolasalevera98@gmail.com>
2023-10-08 02:41:38 -0300

    Fix tool downloader

--- tool-downloader.ps1
+++ tool-downloader.ps1
@@ -1,10 +1,10 @@
-# Exit if 'MultiMonitorTool.exe' exists in the current directory
-if (Test-Path -Path ".\MultiMonitorTool.exe") {
+# Exit if 'MultiMonitorTool.exe' exists in the script's directory
+if (Test-Path -Path "$PSScriptRoot\MultiMonitorTool.exe") {
+    Write-Host "MultiMonitorTool already installed."
     exit 0
 }
 
 # Print message to user
-$userChoice = $null
 $message = "To use this plugin you need to have MultiMonitorTool installed, would you like to install it ?"
 $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Install MultiMonitorTool."
 $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Do not install MultiMonitorTool."
@@ -16,17 +16,17 @@ switch ($result) {
     0 {
         # User chose 'Yes', download the tool
         $url = "https://www.nirsoft.net/utils/multimonitortool-x64.zip"
-        $output = ".\multimonitortool-x64.zip"
+        $output = "$PSScriptRoot\multimonitortool-x64.zip"
         Invoke-WebRequest -Uri $url -OutFile $output
 
         # Extract the tool and move the exe file to the current directory
         Add-Type -AssemblyName System.IO.Compression.FileSystem
-        [System.IO.Compression.ZipFile]::ExtractToDirectory($output, ".\multimonitortool_temp")
-        Move-Item ".\multimonitortool_temp\MultiMonitorTool.exe" ".\MultiMonitorTool.exe"
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($output, "$PSScriptRoot\multimonitortool_temp")
+        Move-Item "$PSScriptRoot\multimonitortool_temp\MultiMonitorTool.exe" "$PSScriptRoot\MultiMonitorTool.exe"
 
         # Clean up, remove the zip file and temporary directory
         Remove-Item -Path $output
-        Remove-Item -Path ".\multimonitortool_temp" -Recurse
+        Remove-Item -Path "$PSScriptRoot\multimonitortool_temp" -Recurse
     }
     1 {
         # User chose 'No', show a message and then exit with error code 1
