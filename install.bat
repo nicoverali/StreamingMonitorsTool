33b53e125e939586cd24c00e1aec837f36de885a
Nicolas A. Vera <nicolasalevera98@gmail.com>
2023-10-08 02:41:55 -0300

    Add ask for admin privilage in install/uninstall.bat

--- install.bat
+++ install.bat
@@ -1,16 +1,32 @@
 @echo off
 
-:: Execute the PowerShell downloader script
-powershell -ExecutionPolicy Bypass -File "%~dp0tool-downloader.ps1"
+:: Check if script is running with admin privileges and elevate if needed
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else (
+    goto gotAdmin
+)
 
-:: If the downloader script fails, exit the batch script
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+:UACPrompt
+    echo Set objShell = CreateObject("Shell.Application") > "%temp%\getadmin.vbs"
+    echo objShell.ShellExecute "%~0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /b
 
-:: If downloader is successful, run the installer script
-powershell -ExecutionPolicy Bypass -File "%~dp0installer.ps1"
+:gotAdmin
+    :: Execute the PowerShell downloader script
+    powershell -ExecutionPolicy Bypass -File "%~dp0tool-downloader.ps1"
 
-:: If the installer script fails, exit the batch script
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+    :: If the downloader script fails, exit the batch script
+    if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-:: Continue the rest of the batch script if both scripts are successful
-pause
+    :: If downloader is successful, run the installer script
+    powershell -ExecutionPolicy Bypass -File "%~dp0installer.ps1"
+
+    :: If the installer script fails, exit the batch script
+    if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+    :: Continue the rest of the batch script if both scripts are successful
+    pause
--- uninstall.bat
+++ uninstall.bat
@@ -1,3 +1,21 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0installer.ps1" -Uninstall
-pause
+
+:: Check if script is running with admin privileges and elevate if needed
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else (
+    goto gotAdmin
+)
+
+:UACPrompt
+    echo Set objShell = CreateObject("Shell.Application") > "%temp%\getadmin.vbs"
+    echo objShell.ShellExecute "%~0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /b
+
+:gotAdmin
+    del "%temp%\getadmin.vbs"
+    powershell.exe -ExecutionPolicy Bypass -File "%~dp0installer.ps1" -Uninstall
+    pause
